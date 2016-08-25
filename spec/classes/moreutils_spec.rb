6a15f286e40c893baec8c5e208952a092d801ed3
Jacob Bednarz <jacob.bednarz@gmail.com>
2016-08-26 09:01:13 +1000

    Deprecate in favour of brewcask
    As mentioned in boxen/boxen#203 brewcask[1] does better job of managing
    simple application installations like this so we are deprecating it in
    favour of using Boxen's built in support[2] for homebrew and brewcask.
    
    [1]: https://caskroom.github.io/
    [2]: https://github.com/boxen/puppet-boxen/blob/master/manifests/personal.pp#L14-L17

--- spec/classes/moreutils_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-
-describe 'moreutils' do
-  let(:pre_condition) { 'class homebrew {}' }
-
-  it { should contain_class('moreutils') }
-  it { should contain_package('moreutils') }
-end
